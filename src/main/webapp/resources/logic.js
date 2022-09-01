47048f5f6c8fc5d92d6103bdac6f8e34f81777aa
Andrea Gonzato <gonzatoandrea98@gmail.com>
2022-09-01 13:12:53 +0200

    import and setting a theme of bootstrap for the home page

--- src/main/webapp/resources/logic.js
+++ src/main/webapp/resources/logic.js
@@ -1,6 +1,14 @@
+
 var vm = new Vue({
     el: '#main',
     data: {
-        firstname: 'Andrea'
+        nameApp: 'Crono Travel Diary'
     }
 })
+
+logHiOnConsole = function(){
+    console.log("Hi");
+}
+
+let loginButton = document.getElementById("loginButton");
+loginButton.addEventListener("click", logHiOnConsole);
